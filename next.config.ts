1473ca65da60fabe14c12cb1e6bd7d2424886738
unknown <abdullaalwaki@icloud.com>
2025-02-18 23:21:53 +0100

    ss=

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // تمكين وضع التصحيح الصارم
-  i18n: {
-    locales: ['en', 'ar'], // اللغات المدعومة
-    defaultLocale: 'en', // اللغة الافتراضية
-  },
+
 };
 
 module.exports = nextConfig; // ✅ استخدام module.exports بدلاً من export default
