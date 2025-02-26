15a27089e2b2378442138fd713311b50ca37c688
Alwaki <91062602+AbdullaAlwaki@users.noreply.github.com>
2025-02-27 00:45:40 +0100

    Update layout.tsx

--- app/[locale]/admin/layout.tsx
+++ app/[locale]/admin/layout.tsx
@@ -3,14 +3,14 @@ import Sidebar from '../../../components-Admin/Sidebar';
 
 export default function AdminLayout({
   children,
-  params, // إضافة params كـ prop
+  params,
 }: {
   children: React.ReactNode;
-  params: { locale: string }; // تعريف نوع params
+  params: Promise<{ locale: string }>; // تحديد النوع كـ Promise
 }) {
   return (
     <div className="max-h-screen bg-gray-50 dark:bg-gray-900 flex">
-      <Sidebar params={params} /> {/* تمرير params إلى Sidebar */}
+      <Sidebar params={params} /> {/* تمرير params كـ Promise */}
       <main className="flex-1 lg:w-4/5">
         <div className="p-4">{children}</div>
       </main>
